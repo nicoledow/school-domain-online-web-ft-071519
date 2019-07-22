8a4969d4243a220025cbeff43dc6ed40cb4c7589
Nicole Dow <nikki.dow71@gmail.com>
2019-07-22 19:26:16 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require 'pry'
+
+class School
+  attr_reader :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if roster.keys.include?(grade)
+      roster[grade] << name
+    else
+      roster[grade] = []
+      roster[grade] << name
+    end
+  end
+  
+  def grade(grade)
+    roster[grade]
+  end
+  
+  def sort
+    roster.each do |grade, array|
+      array.each do 
+        array.sort
+      end
+    end
+  end
+  
+end
